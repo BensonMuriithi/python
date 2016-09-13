cfa83f07282715611471cba43142bcf9a06ce6d9
Benson Muriithi <mwngmuriithi@gmail.com>
2016-09-13 16:59:21 +0300

    New setup version to accomodate changes in reinstall on my pc

--- Accident Game/setup.py
+++ Accident Game/setup.py
@@ -5,9 +5,9 @@ def get_description():
 		return f.read()
 
 setup(name = "accidentgame",
-	version = "0.1",
+	version = "0.1.r1",
 	description = get_description(),
-	url = "https://github.com/BensonMuriithi/python/tree/master/accident_game",
+	url = "https://github.com/BensonMuriithi/python/tree/master/Accident%20Game",
 	author = "Benson Muriithi",
 	author_email = "mwngmuriithi@gmail.com",
 	packages = ["accidentgame"],
